0b9782d1ec69742b48127f1224fd68323003e1cb
Maryna <marinamatkovska0307@gmail.com>
2023-03-10 20:44:59 +0200

    List<>

--- Snake/Snake/Snake.cs
+++ Snake/Snake/Snake.cs
@@ -7,54 +7,41 @@ namespace Snake;
 
 public class Snake
 {
-
-    public static int Func1(int value)
-    {
-        return value;
-    }
-
-    public static int Func2(int value)
-    {
-      return   value = value + 1;
-    }
-
-   
     
     static void Main()
     {
-        int x = 1;
-        Func1(x);
-        Console.WriteLine($"Enter x {x}");
-
-        int y = 12;
-        Func2(y);
-        Console.WriteLine($"Enter y {y}");
-
         
-    Point point1 = new Point(5,7, '*');
+        Point point1 = new Point(5,7, '*');
         point1.Draw();
-        Move(point1, 34,15);
-        Console.WriteLine($"Contain x {point1.x}, y {point1.y}");
-       
-        Point point2 = new Point(9,16,'#');
+        Console.WriteLine();
+        
+        Point point2 = new Point(9,6,'#');
         point2.Draw();
-        Reset(point2);
-        Console.WriteLine($"Contains x {point2.x}, y {point2.y}");
+        Console.WriteLine();
 
-        Console.ReadLine();
+        Point point3 = new Point(3,10,'&');
+        point3.Draw();
+        Console.WriteLine();
 
+        Point point4 = new Point(9,1,'@');
+        point4.Draw();
+        Console.WriteLine();
+        
+        List<Point> points = new List<Point>(4);
+        points.Add(point1);
+        points.Add(point2);
+        points.Add(point3);
+        points.Add(point4);
 
-         static void Move(Point p, int dx, int dy)
+        foreach (var symble in points)
         {
-            p.x = p.x + dx;
-            p.y = p.y + dy;
+            Console.Write($"{symble.symbl}\n");
         }
 
-         static void Reset(Point p)
-         {
-             p = new Point(23,17,'v');
-         }
 
+
+        Console.ReadLine();
+        
     }
    
 }
